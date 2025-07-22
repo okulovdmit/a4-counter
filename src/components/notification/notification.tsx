b2234ac8b392712b10a13a60f22930fedd470572
Dmitrii Okulov <okulovdmit@yandex.ru>
2025-07-22 05:54:20 +0000

    refactor(components/app & notification): added check for file's size and notification

--- src/components/notification/notification.tsx
+++ src/components/notification/notification.tsx
@@ -1,20 +1,20 @@
 type TNotificationProps = {
-  formatError?: boolean;
-  sizeError?: boolean;
-  otherError?: boolean;
+	formatError?: boolean;
+	sizeError?: boolean;
+	otherError?: boolean;
 };
 export const Notification = ({
-  formatError,
-  sizeError,
-  otherError,
+	formatError,
+	sizeError,
+	otherError,
 }: TNotificationProps) => {
-  const format = "Неверный формат. Файл должен быть PDF. Выберите другой файл.";
-  const size = "Файл слишком большой. Максимальный размер 7 МБ.";
-  const other = "Что-то пошло не так, повторите попытку позже";
-  const text = formatError ? format : sizeError ? size : otherError ?? other;
-  return (
-    <div className="notification">
-      <p>{text}</p>
-    </div>
-  );
+	const format = 'Неверный формат. Файл должен быть PDF. Выберите другой файл.';
+	const size = 'Файл слишком большой. Максимальный размер 7 МБ.';
+	const other = 'Что-то пошло не так, повторите попытку позже';
+	const text = formatError ? format : sizeError ? size : otherError ?? other;
+	return (
+		<div className='notification'>
+			<p>{text}</p>
+		</div>
+	);
 };
